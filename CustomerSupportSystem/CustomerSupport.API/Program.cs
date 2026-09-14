68e45030c1b9832e9e87f46237ba8d7d66e8382c
Anit Kumar <anit.kumar@advantive.com>
2026-09-14 16:53:45 +0530

    initial commit

---
+++ CustomerSupportSystem/CustomerSupport.API/Program.cs
@@ -0,0 +1,36 @@
+using CustomerSupport.Infrastructure.Extensions;
+namespace CustomerSupport.API
+{
+  public class Program
+  {
+    public static void Main(string[] args)
+    {
+      var builder = WebApplication.CreateBuilder(args);
+
+      // Add services to the container.
+
+      builder.Services.AddControllers();
+      builder.Services.AddOpenApi();
+
+      var connectionString =
+          builder.Configuration.GetConnectionString("CustomerSupportDBConnection")
+          ?? throw new InvalidOperationException("CustomerSupportDBConnection is not configured.");
+
+      builder.Services.AddInfrastructureServices(connectionString);
+
+      var app = builder.Build();
+
+      // Configure the HTTP request pipeline.
+      if (app.Environment.IsDevelopment())
+      {
+        app.MapOpenApi();
+      }
+
+      app.UseHttpsRedirection();
+
+      app.MapControllers();
+
+      app.Run();
+    }
+  }
+}
