766166a4deebd561f95e34d869c2c5998b7d658f
Anit Kumar <anit.kumar@advantive.com>
2026-09-14 21:22:12 +0530

    Added Repositories

---
+++ CustomerSupportSystem/CustomerSupport.Application/Exceptions/NotFoundException.cs
@@ -0,0 +1,10 @@
+﻿
+namespace CustomerSupport.Application.Exceptions
+{
+  public sealed class NotFoundException : Exception
+  {
+    public NotFoundException(string message) : base(message)
+    {
+    }
+  }
+}
